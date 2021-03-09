6b9ae89d5c25031b9f42c57291d31edb74ad1100
Jure Varlec <jure.varlec@cosylab.com>
2021-03-09 16:01:48 +0100

    Draft implementation of extended timestamp channel filter

--- modules/database/src/std/filters/ts.c
+++ modules/database/src/std/filters/ts.c
@@ -16,20 +16,110 @@
 #include <string.h>
 
 #include "chfPlugin.h"
-#include "db_field_log.h"
 #include "dbExtractArray.h"
 #include "dbLock.h"
+#include "db_field_log.h"
+#include "epicsAssert.h"
 #include "epicsExport.h"
+#include "freeList.h"
 
-/*
- * The size of the data is different for each channel, and can even
- * change at runtime, so a freeList doesn't make much sense here.
- */
-static void freeArray(db_field_log *pfl) {
-    free(pfl->u.r.field);
+/* Allocation size for freelists */
+#define ALLOC_NUM_ELEMENTS 32
+
+
+/* Filter settings */
+
+enum tsMode {
+    tsModeInvalid = 0,
+    tsModeGenerate = 1,
+    tsModeFloat = 2,
+    tsModeSec = 3,
+    tsModeNsec = 4,
+    tsModeArray = 5,
+    tsModeFmt = 6,
+};
+
+static const chfPluginEnumType ts_numeric_enum[] = {
+    {"float", 2}, {"sec", 3}, {"nsec", 4}, {"ts", 5}};
+
+enum tsEpoch {
+    tsEpochEpics = 0,
+    tsEpochUnix = 1,
+};
+
+static const chfPluginEnumType ts_epoch_enum[] = {{"epics", 0}, {"unix", 1}};
+
+typedef struct tsPrivate {
+    enum tsMode mode;
+    enum tsEpoch epoch;
+    char fmt[MAX_STRING_SIZE];
+} tsPrivate;
+
+static const chfPluginArgDef ts_args[] = {
+    chfEnum(tsPrivate, mode, "num", 0, 0, ts_numeric_enum),
+    chfEnum(tsPrivate, epoch, "epoch", 0, 0, ts_epoch_enum),
+    chfString(tsPrivate, fmt, "fmt", 0, 0),
+    chfPluginArgEnd
+};
+
+static int parse_finished(void *pvt) {
+  tsPrivate *settings = (tsPrivate *)pvt;
+  if (settings->fmt[0] != 0) {
+    settings->mode = tsModeFmt;
+    settings->fmt[MAX_STRING_SIZE - 1] = 0;
+    // TODO check fmt string
+  } else if (settings->mode == tsModeInvalid) {
+    settings->mode = tsModeGenerate;
+  }
+  return 0;
 }
 
-static db_field_log* filter(void* pvt, dbChannel *chan, db_field_log *pfl) {
+
+/* Allocation of filter settings */
+static void *private_free_list;
+
+static void * allocPvt() {
+    return freeListCalloc(private_free_list);
+}
+
+static void freePvt(void *pvt) {
+    freeListFree(private_free_list, pvt);
+}
+
+
+/* Allocation of two-element arrays for second+nanosecond pairs */
+static void *ts_array_free_list;
+
+static void *allocTsArray() {
+    return freeListCalloc(ts_array_free_list);
+}
+
+static void freeTsArray(db_field_log *pfl) {
+    freeListFree(ts_array_free_list, pfl->u.r.field);
+}
+
+/* Allocation of strings */
+static void *string_free_list;
+
+static void *allocString() {
+    return freeListCalloc(string_free_list);
+}
+
+static void freeString(db_field_log *pfl) {
+    freeListFree(string_free_list, pfl->u.r.field);
+}
+
+
+/* The dtor for waveform data for the case when we have to copy it. */
+static void freeArray(db_field_log *pfl) {
+  /*
+   * The size of the data is different for each channel, and can even
+   * change at runtime, so a freeList doesn't make much sense here.
+   */
+  free(pfl->u.r.field);
+}
+
+static db_field_log* generate(void* pvt, dbChannel *chan, db_field_log *pfl) {
     epicsTimeStamp now;
     epicsTimeGetCurrent(&now);
 
@@ -56,34 +146,185 @@ static db_field_log* filter(void* pvt, dbChannel *chan, db_field_log *pfl) {
     return pfl;
 }
 
-static void channelRegisterPre(dbChannel *chan, void *pvt,
-                               chPostEventFunc **cb_out, void **arg_out, db_field_log *probe)
-{
+static db_field_log *replace_fl_value(tsPrivate const *pvt,
+                                      db_field_log *pfl,
+                                      void (*func)(tsPrivate const *,
+                                                   db_field_log *)) {
+    /* Get rid of the old value */
+    if (pfl->type == dbfl_type_ref && pfl->u.r.dtor) {
+        pfl->u.r.dtor(pfl);
+    }
+
+    /* Set up the most common type. We can't support unsigned integers, use
+       doubles instead */
+    pfl->field_type = DBF_DOUBLE;
+    pfl->field_size = sizeof(epicsFloat64);
+    pfl->no_elements = 1;
+    pfl->type = dbfl_type_val;
+
+    func(pvt, pfl);
+    return pfl;
+}
+
+static void ts_to_array(tsPrivate const *settings,
+                 epicsTimeStamp const *ts,
+                 epicsUInt32 arr[2]) {
+    arr[0] = ts->secPastEpoch;
+    arr[1] = ts->nsec;
+    if (settings->epoch == tsEpochUnix) {
+        /* Cannot use epicsTimeToWhatever because Whatever uses signed ints */
+        arr[0] += POSIX_TIME_AT_EPICS_EPOCH;
+    }
+}
+
+static void ts_seconds(tsPrivate const *settings, db_field_log *pfl) {
+    epicsUInt32 arr[2];
+    ts_to_array(settings, &pfl->time, arr);
+    pfl->u.v.field.dbf_double = arr[0];
+}
+
+static void ts_nanos(tsPrivate const *settings, db_field_log *pfl) {
+    epicsUInt32 arr[2];
+    ts_to_array(settings, &pfl->time, arr);
+    pfl->u.v.field.dbf_double = arr[1];
+}
+
+static void ts_float(tsPrivate const *settings, db_field_log *pfl) {
+    epicsUInt32 arr[2];
+    ts_to_array(settings, &pfl->time, arr);
+    pfl->u.v.field.dbf_double = arr[0] + arr[1] * 1e-9;
+}
+
+static void ts_array(tsPrivate const *settings, db_field_log *pfl) {
+    epicsUInt32 arr[2];
+    ts_to_array(settings, &pfl->time, arr);
+    pfl->no_elements = 2;
+    pfl->type = dbfl_type_ref;
+    pfl->u.r.pvt = NULL;
+    pfl->u.r.field = allocTsArray();
+    pfl->u.r.dtor = freeTsArray;
+    epicsFloat64 *farr = pfl->u.r.field;
+    farr[0] = arr[0];
+    farr[1] = arr[1];
+}
+
+static void ts_fmt(tsPrivate const *settings, db_field_log *pfl) {
+    assert(settings->fmt[MAX_STRING_SIZE - 1] == 0);
+    // TODO
+    static char const *nimpl = "NOT IMPLEMENTED";
+    pfl->field_type = DBF_STRING;
+    pfl->field_size = MAX_STRING_SIZE;
+    pfl->type = dbfl_type_ref;
+    pfl->u.r.pvt = NULL;
+    pfl->u.r.field = allocString();
+    pfl->u.r.dtor = freeString;
+    strcpy(pfl->u.r.field, nimpl);
+}
+
+static db_field_log *filter(void *pvt, dbChannel *chan, db_field_log *pfl) {
+    tsPrivate *settings = (tsPrivate *)pvt;
+
+    switch (settings->mode) {
+    case tsModeFloat:
+        return replace_fl_value(pvt, pfl, ts_float);
+    case tsModeSec:
+        return replace_fl_value(pvt, pfl, ts_seconds);
+    case tsModeNsec:
+        return replace_fl_value(pvt, pfl, ts_nanos);
+    case tsModeArray:
+        return replace_fl_value(pvt, pfl, ts_array);
+    case tsModeFmt:
+        return replace_fl_value(pvt, pfl, ts_fmt);
+    case tsModeGenerate:
+    case tsModeInvalid:
+        assert(0);
+    }
+
+    return pfl;
+}
+
+
+/* Only the "generate" mode is registered for the pre-queue chain as it creates
+   it's own timestamp which should be as close to the event as possible */
+static void channelRegisterPre(dbChannel * chan, void *pvt,
+                                 chPostEventFunc **cb_out, void **arg_out,
+                                 db_field_log *probe) {
+    tsPrivate *settings = (tsPrivate *)pvt;
+    *cb_out = settings->mode == tsModeGenerate ? generate : NULL;
+}
+
+/* For other modes, the post-chain is fine as they only manipulate existing
+   timestamps */
+static void channelRegisterPost(dbChannel *chan, void *pvt,
+                               chPostEventFunc **cb_out, void **arg_out,
+                               db_field_log *probe) {
+    tsPrivate *settings = (tsPrivate *)pvt;
+
+    if (settings->mode == tsModeGenerate || settings->mode == tsModeInvalid) {
+        *cb_out = NULL;
+        return;
+    }
+
     *cb_out = filter;
+    *arg_out = pvt;
+
+    /* Get rid of the value of the probe because we will be changing the
+       datatype */
+    if (probe->type == dbfl_type_ref && probe->u.r.dtor) {
+        probe->u.r.dtor(probe);
+    }
+    probe->no_elements = 1;
+    probe->type = dbfl_type_val;
+
+    switch (settings->mode) {
+    case tsModeArray:
+        probe->no_elements = 2;
+        /* fallthrough */
+    case tsModeSec:
+    case tsModeNsec:
+    case tsModeFloat:
+        probe->field_type = DBF_DOUBLE;
+        probe->field_size = sizeof(epicsFloat64);
+        break;
+    case tsModeFmt:
+        probe->field_type = DBF_STRING;
+        probe->field_size = MAX_STRING_SIZE;
+        break;
+    case tsModeGenerate:
+    case tsModeInvalid:
+        assert(0);
+    }
 }
 
 static void channel_report(dbChannel *chan, void *pvt, int level, const unsigned short indent)
 {
+    // TODO
     printf("%*sTimestamp (ts)\n", indent, "");
 }
 
 static chfPluginIf pif = {
-    NULL, /* allocPvt, */
-    NULL, /* freePvt, */
+    allocPvt,
+    freePvt,
 
-    NULL, /* parse_error, */
-    NULL, /* parse_ok, */
+    NULL,           /* parse_error, */
+    parse_finished,
 
     NULL, /* channel_open, */
     channelRegisterPre,
-    NULL, /* channelRegisterPost, */
+    channelRegisterPost,
     channel_report,
     NULL /* channel_close */
 };
 
 static void tsInitialize(void)
 {
-    chfPluginRegister("ts", &pif, NULL);
+    freeListInitPvt(&private_free_list, sizeof(tsPrivate),
+                    ALLOC_NUM_ELEMENTS);
+    freeListInitPvt(&ts_array_free_list, 2 * sizeof(epicsFloat64),
+                    ALLOC_NUM_ELEMENTS);
+    freeListInitPvt(&string_free_list, MAX_STRING_SIZE,
+                    ALLOC_NUM_ELEMENTS);
+    chfPluginRegister("ts", &pif, ts_args);
 }
 
 epicsExportRegistrar(tsInitialize);
